855465784a45d3bdd93b45818421eb3d6a28388b
Jagadeesh <99111481+JagadeeshSmash@users.noreply.github.com>
2024-08-11 12:02:21 +0530

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,91 @@
+//Carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    smartSpeed:1000,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+  })
+  
+/*Form Validation*/
+function validation(){
+   const name= document.getElementById("name").value
+   const email= document.getElementById("email").value
+   const mobile=document.getElementById("mobile").value
+
+   const nameStatus=document.getElementById("nameStatus")
+   const emailStatus=document.getElementById("emailStatus")
+   const mobileStatus=document.getElementById("mobileStatus")
+
+   let sname=false
+   let eemail=false
+   let emobile=false
+
+   const namem= /^[a-zA-Z]+$/
+   const emailm=/^\w+([.-]?\w+)*@\w+([.-]?\w+)*\.\w{2,3}$/
+   const mobilem=/^[0-9]+$/
+
+    
+   //Name
+   if(name===""){
+    nameStatus.innerHTML="Enter Your Name*"
+   }else{
+    if(name.match(namem)){
+        nameStatus.innerHTML=""
+        sname=true
+    }else{
+        nameStatus.innerHTML="Only Character's Are Allowed"
+    }
+   }
+
+   //Email
+   if(email===""){
+    emailStatus.innerHTML="Enter Your Email*"
+   }else{
+    if(email.match(emailm)){
+        emailStatus.innerHTML=""
+        eemail=true
+    }else{
+        emailStatus.innerHTML="Enter Your Email Properly"
+    }
+   }
+
+   //Mobile
+   if(mobile===""){
+    mobileStatus.innerHTML="Enter Your Mobile Number*"
+   }else{
+    if(mobile.match(mobilem)){
+        mobileStatus.innerHTML=""
+        if(mobile.length==10){
+            emobile=true
+        }else{
+            mobileStatus.innerHTML="Enter Your 10 Digits Number"
+        }
+    }else{
+        mobileStatus.innerHTML="Only Digits Are Allowed"
+    }
+   }
+
+    //Return
+    if(sname===true&&eemail===true&&emobile===true){
+        const modal=new bootstrap.Modal(document.getElementById("submitres"))
+        modal.show()
+        return true;
+    }else{
+        return false;
+    }
+}
+
